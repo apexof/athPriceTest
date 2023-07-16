3043c6cddf304b9fecf6319b701747db7c979235
AlexOrekhov <apexof@gmail.com>
2023-07-16 15:02:02 +0300

    remove api key

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,11 +2,7 @@ import axios, { AxiosError } from "axios";
 
 export const fetcher = async (url: string) => {
   try {
-    const response = await axios.get(url, {
-      headers: {
-        api_key: process.env.NEXT_PUBLIC_CRYPTO_RANK_API_KEY,
-      },
-    });
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     const axiosError = error as AxiosError;

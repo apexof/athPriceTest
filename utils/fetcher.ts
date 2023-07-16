91dbfe1c5673095639946eb32ec6da7ee6cb659a
AlexOrekhov <apexof@gmail.com>
2023-07-16 11:47:23 +0300

    add api key

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,10 +2,13 @@ import axios, { AxiosError } from "axios";
 
 export const fetcher = async (url: string) => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        API_KEY: process.env.NEXT_PUBLIC_CRYPTO_RANK_API_KEY,
+      },
+    });
     return response.data;
   } catch (error) {
-    console.log("error", error);
     const axiosError = error as AxiosError;
     throw new Error(axiosError.message);
   }
